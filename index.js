36295772a1102c8cd45aa14abf820bf01812e083
Jan <pw.jan.karlo.bulawan@gmail.com>
2022-04-02 09:42:32 +0800

    Update index.js

--- index.js
+++ index.js
@@ -50,5 +50,5 @@ function hashPassword(password){
 }
 
 // PORT
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
