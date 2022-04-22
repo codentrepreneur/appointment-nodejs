351ac48a1369218162c55b3d7133128b87a4c009
Jan <pw.jan.karlo.bulawan@gmail.com>
2022-04-22 14:21:22 +0800

    Update corrs.js

--- middleware/corrs.js
+++ middleware/corrs.js
@@ -6,7 +6,7 @@ function corrs(req, res, next){
 
     // Website you wish to allow to connect
     //res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-    res.setHeader('Access-Control-Allow-Origin', 'https://appointment.jankarlobulawan.com');
+    res.setHeader('Access-Control-Allow-Origin', 'http://appointment.jankarlobulawan.com');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
